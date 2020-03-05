625f734a14584f27a7f13e18a7b30eb6b6308297
David Calavera <david.calavera@gmail.com>
2020-03-05 11:06:02 -0800

    Provide access to TSRemapRequestInfo in RemapPlugins.
    Add a new virtual method that gives you more control over
    how a remap happens in a RemapPlugin.
    
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- lib/cppapi/include/atscppapi/RemapPlugin.h
+++ lib/cppapi/include/atscppapi/RemapPlugin.h
@@ -25,6 +25,7 @@
 #include "atscppapi/Transaction.h"
 #include "atscppapi/Url.h"
 #include "atscppapi/utils.h"
+#include "ts/remap.h"
 
 namespace atscppapi
 {
@@ -50,6 +51,25 @@ public:
   };
 
   /**
+   * Invoked when a request matches the remap.config line - it gives you access to the remap information
+   * if you want to have more control over how the remap happens.
+   *
+   * @param transaction Transaction
+   * @param rri The remap information in the remap.config line.
+   *
+   * @return Result of the remap - will dictate further processing by the system.
+   */
+  virtual Result
+  remapTransaction(Transaction &transaction, TSRemapRequestInfo *rri)
+  {
+    Url map_from_url(rri->requestBufp, rri->mapFromUrl), map_to_url(rri->requestBufp, rri->mapToUrl);
+    bool redirect              = false;
+    RemapPlugin::Result result = doRemap(map_from_url, map_to_url, transaction, redirect);
+    rri->redirect              = redirect ? 1 : 0;
+    return result;
+  }
+
+  /**
    * Invoked when a request matches the remap.config line - implementation should perform the
    * remap. The client's URL is in the transaction and that's where it should be modified.
    *
@@ -70,4 +90,4 @@ public:
 
   virtual ~RemapPlugin() {}
 };
-}
+} // namespace atscppapi
